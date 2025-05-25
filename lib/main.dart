a4b85445596b1c6ffd2a4df7d2bcccd90853fffa
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-05-25 16:44:27 +0600

    Add initial implementation of weather app features
    - Created color constants in `app_colors.dart` for consistent theming.
    - Defined application constants in `constants.dart` for API keys.
    - Implemented text styles in `text_styles.dart` for uniform typography.
    - Added date and time formatting extensions in `datetime.dart`.
    - Introduced wind speed conversion extension in `double.dart`.
    - Created timestamp conversion extension in `int.dart`.
    - Added string capitalization extension in `strings.dart`.
    - Developed models for hourly and weekly weather data in `hourly_weather.dart` and `weekly_weather.dart`.
    - Implemented weather data model in `weather.dart` for API response handling.
    - Added utility functions for weather icon retrieval in `get_weather_icons.dart`.
    - Implemented logging utilities in `logging.dart` for better debugging.
    - Generated CMake and metadata files for Android build configuration.
    - Created prefab configuration for project dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:weatherwise/view/splash/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (_ , child) {
-        return GetMaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'WeatherWise',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
-          ),
-          home: const SplashScreen(),
-        );
-      },
-
+    return const MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            Center(
+              child: Text('Hello World!'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
