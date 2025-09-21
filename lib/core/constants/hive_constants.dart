fe05aa24a8c0fe4b14292093d26d7cdc838ca2a0
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-09-21 16:28:49 +0600

    Add theme management and color constants for light and dark modes

---
+++ lib/core/constants/hive_constants.dart
@@ -0,0 +1,15 @@
+class HiveConstants {
+  //! hive box names
+  static const String favoriteLocationsBoxName = 'favoritesBox';
+  static const String prefBoxName = 'prefBox';
+  static const String weatherDataBoxName = 'weatherDataBox';
+  static const String hasDataFetchedOnceBoxName = 'hasDataFetchedOnceBox';
+
+
+  //! hive keys
+  static const String themeKey = 'theme';
+  static const String unitKey = 'unit';
+  static const String weatherDataKey = 'weatherdata';
+  static const String hasDataFetchedOnceKey = 'hasDataFetchedOnce';
+  static const String favoriteLocationsKey = 'favoriteLocations';
+}
