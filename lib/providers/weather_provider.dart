bea09be4b57b3ce239fdfd065d62491535bdd208
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-09-29 10:07:22 +0600

    Add weather models and data classes for current, hourly, and daily weather

--- lib/providers/weather_provider.dart
+++ lib/providers/weather_provider.dart
@@ -1,4 +1,19 @@
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:weatherwise/core/constants/hive_constants.dart';
+import 'package:weatherwise/models/weather_model.dart';
+import 'package:weatherwise/providers/internet_provider.dart';
+import 'package:weatherwise/providers/search_provider.dart';
 
-// final weatherProvider=FutureProvider()
+final weatherProvider = FutureProvider<WeatherData?>((ref) async {
+  final searchState = ref.read(searchProvider);
+  final internetState = ref.read(internetStatusProvider);
+
+  //! Hive Boxes
+  final weatherDataBox =
+      Hive.box<WeatherData>(HiveConstants.weatherDataBoxName);
+  final hasDataFetchedOnceBox =
+      Hive.box<bool>(HiveConstants.hasDataFetchedOnceBoxName);
+
+  WeatherData? weatherData;
+});
