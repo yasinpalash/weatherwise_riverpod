676c4048c2e60d69a8fa0288ea8c8cb769f42eb8
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-12-03 17:13:50 +0600

    feat: Implement search screen UI
    - Create the initial user interface for the `SearchScreen`.
    - Add a `TextField` for user input, complete with a location icon, hint text, and a clear button.
    - Include a descriptive text prompt and an `AppBar`.
    - Convert `SearchScreen` from a `ConsumerWidget` to a `ConsumerStatefulWidget` to manage the `TextEditingController` state.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,14 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SearchScreen extends ConsumerWidget {
+class SearchScreen extends ConsumerStatefulWidget {
   const SearchScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search Screen'),
+  ConsumerState<ConsumerStatefulWidget> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends ConsumerState<SearchScreen> {
+  final _inputController = TextEditingController();
+  void _performSearch( String value){
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Search Location'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  'Search for a location you want to know the current weather of',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                            controller: _inputController,
+                            decoration: InputDecoration(
+                              hintText: 'Search...',
+                              prefixIcon:
+                                  const Icon(Icons.location_on_outlined),
+                              suffixIcon: _inputController.text.isNotEmpty
+                                  ? IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.clear,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                        size: 20,
+                                      ))
+                                  : null,
+                              border: const OutlineInputBorder(),
+                            )))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
