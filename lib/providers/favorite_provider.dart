421565013b4dd38ba0ef58e53af3230b257ecfa2
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-12-09 12:58:12 +0600

    add fav

--- lib/providers/favorite_provider.dart
+++ lib/providers/favorite_provider.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:weatherwise/models/favorite_location.dart';
+import '../core/constants/hive_constants.dart';
 
+final favoriteProvider =
+    StateNotifierProvider<FavoriteLocationNotifier, List<FavoriteLocation>>(
+        (ref) {
+  return FavoriteLocationNotifier();
+});
+
+class FavoriteLocationNotifier extends StateNotifier<List<FavoriteLocation>> {
+  FavoriteLocationNotifier() : super([]) {
+    _loadFavoriteLocationsFromDB();
+  }
+  void removeFavorite(FavoriteLocation fav) {
+    state = state.where((ele) => ele != fav).toList();
+    _saveFavoriteLocationInDB(state);
+  }
+  //! Hive-related operations
+  final favoriteLocationBox =
+      Hive.box<List<FavoriteLocation>>(HiveConstants.favoriteLocationsBoxName);
+  void _loadFavoriteLocationsFromDB() {
+    final favorites = favoriteLocationBox.values.cast<List<FavoriteLocation>>();
+    final favs = favorites.toList().expand((element) => element).toList();
+
+    if (favs.isEmpty) {
+      state = [];
+    } else {
+      state = favs;
+    }
+    debugPrint('Loaded $favs');
+  }
+
+  void _saveFavoriteLocationInDB(List<FavoriteLocation> favs) {
+    favoriteLocationBox.put(HiveConstants.favoriteLocationsKey, favs);
+    debugPrint('Saved $favs');
+  }
+
+}
--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import '../providers/favorite_provider.dart';
 import '../providers/internet_provider.dart';
 
 class FavoriteScreen extends ConsumerWidget {
@@ -14,11 +14,71 @@ class FavoriteScreen extends ConsumerWidget {
     ),
   );
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-   // final favoriteLocations = ref.watch(favoriteProvider);
+    final favoriteLocations = ref.watch(favoriteProvider);
     final internetState = ref.watch(internetStatusProvider);
-    return const Center(child: Text('Favorite Screen'));
+    return Scaffold(
+      backgroundColor:
+          Theme.of(context).colorScheme.onSurfaceVariant.withValues(alpha: .1),
+      appBar: AppBar(
+        title: const Text('Favorites'),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SafeArea(
+          child: favoriteLocations.isEmpty
+              ? noFavorites
+              : ListView.builder(
+                  itemCount: favoriteLocations.length,
+                  itemBuilder: (context, index) {
+                    final location = favoriteLocations[index];
+
+                    return Dismissible(
+                      key: ValueKey(location),
+                      onDismissed: (direction) {
+                        ref
+                            .read(favoriteProvider.notifier)
+                            .removeFavorite(location);
+                      },
+                      background: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.only(right: 10, left: 10),
+                        margin: const EdgeInsets.only(left: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.red.withOpacity(.4),
+                        ),
+                        child: const Icon(Icons.delete),
+                      ),
+                      child: FavoriteTile(
+                        location: location,
+                        onTap: () async {
+                          if (await internetState.isConnected()) {
+                            ref
+                                .read(searchProvider.notifier)
+                                .performSearch(location.lat, location.lon);
+                            ref.refresh(locationProvider);
+                            ref.refresh(screenIndexProvider.notifier).state = 0;
+                            ref.refresh(weatherProvider);
+                          } else {
+                            ScaffoldMessenger.of(context).clearSnackBars();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                duration: const Duration(seconds: 2),
+                                content: const Text(
+                                    'Please connect to internet and try again!'),
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.error,
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    );
+                  },
+                )),
+    );
   }
 }
